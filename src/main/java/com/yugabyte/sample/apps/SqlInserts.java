505b363d45c00fe2783c662f26fa7d321de029d5
Isha Amoncar <iamoncar@yugabyte.com>
2022-04-01 10:13:22 +0000

    Modify schema

--- src/main/java/com/yugabyte/sample/apps/SqlInserts.java
+++ src/main/java/com/yugabyte/sample/apps/SqlInserts.java
@@ -53,7 +53,7 @@ public class SqlInserts extends AppBase {
         return super.getKeyspace();
     }
 
-    private static final String DEFAULT_TABLE_DEFN = "CREATE TABLE api_watch_list\n"
+    private static final String DEFAULT_TABLE_DEFN = "CREATE TABLE IF NOT EXISTS api_watch_list\n"
             + "(\n"
             + "  k                       text PRIMARY KEY,\n"
             + "  v                       text ,\n"
